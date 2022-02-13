6392b8c9c94f17ad7ed846107a17873eef02787d
Anthonin Colas <Sterben@cefimmac97.home>
2022-02-13 18:01:32 +0100

    api mis en place reste plus qu'a regler le probleme d'affichage qui reste stuck

--- js/api-coronavirus.js
+++ js/api-coronavirus.js
@@ -1,25 +1,28 @@
 let country = document.querySelector("#country")
 let map = new Map;
-country.addEventListener("input", function(){
-    if (country.value == "France"){
+let search = document.querySelector("#search")
+function searchingResult(){
+    let url = `https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total?country=${country.value}`;
 
-        let url = `https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total?country=${country.value}`;
+    fetch(url, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+            "x-rapidapi-key": "bcf21581aamsh871a6b42f118933p161d62jsn2c24c88dd6e5"
+        }
+    }).then((response) => response.json().then((data) => {
+        console.log(data)
+        let mapped = map.set(data).entries().next().value;
+        let resultMap = mapped[0].data;
+            document.querySelector("#location").textContent = `${resultMap.location}`
+            document.querySelector("#Contamination").innerHTML = `${resultMap.confirmed} Contaminés`;
+            document.querySelector("#Morts").innerHTML = `${resultMap.deaths} Morts`;
 
-        fetch(url, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
-                "x-rapidapi-key": "bcf21581aamsh871a6b42f118933p161d62jsn2c24c88dd6e5"
-            }
-        }).then((response) => response.json().then((data) => {
-            console.log(data)
-            let mapped = map.set(data).entries().next().value;
-            let resultMap = mapped[0].data;
-            document.querySelector("#location").innerHTML = `${resultMap.location}`;
+    }))
+}
 
-                console.log(resultMap.deaths)
-        }))
-    }   
-});
+
+search.addEventListener("click", searchingResult)
+
 
 
